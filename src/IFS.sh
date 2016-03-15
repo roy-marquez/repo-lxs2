5e3b3a0434e5e5fd1d05115baf56d76d2e81537d
Emmanuel Vargas B <emvargcr@gmail.com>
2016-03-15 16:05:52 -0600

    Ejemplo para correr con env VAR2 y VAR3
    Este ejemplo se debe correr así
    
    $ env VAR2=123 VAR3=abc bash IFS.sh
    
    La otra forma de correrlo es cargando las variables en bash
    y exportandolas para que el script las agarre
    
    $ VAR2=123
    $ VAR3=abc
    $ export VAR2 VAR3
    $ bash IFS.sh

--- src/IFS.sh
+++ src/IFS.sh
@@ -9,5 +9,11 @@ IFS=:,
 
 for dato in $VAR1; do
 	echo "Dato: $dato"
+	
+	# Para correr este ejemplo se deben definir las variables
+	# VAR2 y VAR3 antes de correr el script. Hagalo de dos
+	# formas distitas sin definirlas dentro del script
+	echo "La variable dos contiene \"$VAR2\" y \
+	tres contiene \"$VAR3\"."
 done
 
